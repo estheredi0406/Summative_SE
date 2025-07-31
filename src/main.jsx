1c318077207805c537da444c3cdab89e575a0cc2
Edi-champion <estherafi0406@gmail.com>
2025-07-31 07:25:50 +0200

    name chnged

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { CartProvider } from "./context/Cartcontext"; // ✅ import this
+import { CartProvider } from "./context/Cartcontext"; //
 import App from "./App.jsx";
 import "./index.css";
 
@@ -10,7 +10,7 @@ createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <CartProvider>
         {" "}
-        {/* ✅ wraps your app */}
+        {/*  */}
         <App />
       </CartProvider>
     </BrowserRouter>
